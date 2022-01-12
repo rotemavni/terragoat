7f8d658957dda8a04e94667c116d8cef4d0b4497
Rotem Avni <52502521+rotemavni@users.noreply.github.com>
2022-01-12 19:22:34 +0200

    Update or.tf

--- or.tf
+++ or.tf
@@ -4,3 +4,10 @@ resource "aws_s3_bucket" "example1" {
   acl = "public-read"
   force_destroy = true
 }
+
+resource "aws_s3_bucket" "example2" {
+  bucket = "${aws_s3_bucket.example.id}"
+  region = "us-west-2"
+  acl = "public-read"
+  force_destroy = true
+}
